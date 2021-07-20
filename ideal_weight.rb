992c91db18588fb9fa47a3075b39721762c91050
imepanov <87714341+imepanov@users.noreply.github.com>
2021-07-20 23:05:47 +0500

    Add files via upload

---
+++ ideal_weight.rb
@@ -0,0 +1,10 @@
+puts "Как тебя зовут?"
+name = gets.chomp
+
+puts "Какой у тебя рост?"
+height = gets.chomp
+
+if (height.to_i - 110) * 1.15 > 0
+  puts "#{name}, привет! Твой идеальный вес #{(height.to_i - 110) * 1.15}"
+else puts "Ваш вес уже оптимальный"
+end
