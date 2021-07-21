a4870285fe956ca8df69415fa394974ee6672393
imepanov <87714341+imepanov@users.noreply.github.com>
2021-07-21 20:21:05 +0500

    Add files via upload

--- rectangular.rb
+++ rectangular.rb
@@ -1,19 +1,14 @@
 puts "Введите 3 стороны треугольника"
-a = gets.chomp
-b = gets.chomp
-c = gets.chomp
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+c = gets.chomp.to_i
+[a, b, c].sort
 if a == b && a == c
   puts "треугольник равносторонний"
 end
 if a == b || b == c || a == c
   puts "треугольник равнобедренный"
 end
-if a.to_i > b.to_i && a.to_i > c.to_i && (a.to_i ** 2 - b.to_i ** 2 - c.to_i ** 2) == 0
-  puts "треугольник прямоугольный"
-end
-if b.to_i > a.to_i && b.to_i > c.to_i && (b.to_i ** 2 - a.to_i ** 2 - c.to_i ** 2) == 0
-  puts "треугольник прямоугольный"
-end
-if c.to_i > a.to_i  && c.to_i > b.to_i && (c.to_i ** 2 - b.to_i ** 2 - a.to_i ** 2) == 0
+if a > b && a > c && (a ** 2 - b ** 2 - c ** 2) == 0
   puts "треугольник прямоугольный"
 end
