2c0a79ad6a66c296cfc40dbc99872155f5cec9b7
imepanov <87714341+imepanov@users.noreply.github.com>
2021-07-21 20:21:40 +0500

    Add files via upload

--- equation.rb
+++ equation.rb
@@ -1,12 +1,12 @@
 puts "введите коэффициенты a, b, c"
-a = gets.chomp
-b = gets.chomp
-c = gets.chomp
-d = (b.to_i ** 2) - (4 * a.to_i * c.to_i)
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+c = gets.chomp.to_i
+d = (b ** 2) - (4 * a * c)
 if d < 0
   puts "Дискриминант равен #{d}, корней нет!"
 elsif d == 0
-  puts "Дискриминант равен #{d}, Х равен #{-b.to_i / 2 * a.to_i}"
+  puts "Дискриминант равен #{d}, Х равен #{-b / 2 * a}"
 else d > 0
-  puts "Дискриминант равен #{d}, Х1 равен #{(-b.to_i + Math.sqrt(d)) / (2 * a.to_i)}, Х2 равен #{(-b.to_i - Math.sqrt(d)) / (2 * a.to_i)}"
+  puts "Дискриминант равен #{d}, Х1 равен #{(-b + Math.sqrt(d)) / (2 * a)}, Х2 равен #{(-b- Math.sqrt(d)) / (2 * a)}"
 end
